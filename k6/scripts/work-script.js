2b3d0e1ec3d8e3e3fe86c6fdc483c22dbcccfc3c
Arseni Leskinen <wd975700@varian.com>
2023-02-10 00:03:06 +0200

    Use port 8081 as this is also used in k3d config

--- k6/scripts/work-script.js
+++ k6/scripts/work-script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.post('http://host.docker.internal:8080/Work');
+  http.post('http://host.docker.internal:8081/Work');
   sleep(1);
 }
